2436a87e02c0b45e3fc689828b38e85ca28e469c
vldrshv@gmail.com <Poiu0987654321>
2019-06-22 19:50:22 +0300

    terms of use and privacy policy added

---
+++ prilogulka/app/src/main/java/com/example/prilogulka/menu/AboutAppActivity.java
@@ -0,0 +1,60 @@
+package com.example.prilogulka.menu;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.prilogulka.R;
+
+public class AboutAppActivity extends AppCompatActivity implements Button.OnClickListener{
+
+    Button buttonTermsOfUse, buttonPrivacyPolicy;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_app);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        buttonPrivacyPolicy = findViewById(R.id.buttonPrivacyPolicy);
+        buttonTermsOfUse = findViewById(R.id.buttonTermsOfUse);
+        buttonPrivacyPolicy.setOnClickListener(this);
+        buttonTermsOfUse.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v){
+        String uri = "";
+        switch(v.getId()) {
+            case R.id.buttonTermsOfUse:
+                uri = "https://docs.google.com/document/d/1aBNQP2mH7kXOg2aVJdbWgeu_ezxP6THWeYG2Ev75Pow/edit?usp=sharing";
+                break;
+            case R.id.buttonPrivacyPolicy:
+                uri = "https://docs.google.com/document/d/15Zdd2byTbWrjsb94NYJtgl8ylEzffDDzTjVqVAzOTpc/edit?usp=sharing";
+                break;
+        }
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+        startActivity(browserIntent);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public void onBackPressed(){
+        getSupportFragmentManager().popBackStack();
+        super.onBackPressed();
+    }
+}
